4adf1058a5d11601c577b728e734d05746438972
ForzaIs-Fav9 <89690651+ForzaIs-Fav9@users.noreply.github.com>
2025-12-25 15:32:45 +0530

    Render reactants and products from AST

--- src/render.js
+++ src/render.js
@@ -3,14 +3,18 @@ import { parseMechlang } from "./parse.js";
 
 const input = fs.readFileSync("examples/sn2.mech", "utf-8");
 const ast = parseMechlang(input);
+const [r1, r2] = ast.reaction.reactants;
+const [p1, p2] = ast.reaction.products;
+
 
 // Very simple SVG layout (hardcoded)
 const svg = `
 <svg width="500" height="200" xmlns="http://www.w3.org/2000/svg">
 
   <!-- Reactants -->
-  <text x="100" y="100" font-size="16">CH3-Br</text>
-  <text x="40" y="80" font-size="16">OH⁻</text>
+  <text x="100" y="100">${r1}</text>
+<text x="40" y="80">${r2}</text>
+
 
   <!-- Curved arrow -->
   <path d="M 60 85 Q 100 40 120 95"
@@ -18,8 +22,9 @@ const svg = `
         marker-end="url(#arrowhead)" />
 
   <!-- Products -->
-  <text x="300" y="100" font-size="16">CH3-OH</text>
-  <text x="340" y="80" font-size="16">Br⁻</text>
+ <text x="300" y="100">${p1}</text>
+<text x="340" y="80">${p2}</text>
+
 
   <!-- Arrowhead definition -->
   <defs>
